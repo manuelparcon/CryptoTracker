fa396e79a3f302257235b39e7e0ff0831c3c7cb2
Manuel <manuel@manuelparcon.com>
2024-11-11 11:28:22 +0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
