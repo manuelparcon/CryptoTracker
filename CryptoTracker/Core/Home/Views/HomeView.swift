e7b1765090b584cbe5a943596c25165726a61c78
Manuel <manuel@manuelparcon.com>
2024-11-12 00:07:39 +0800

    Add HomeView including CircleButtonView and CircleButtonAnimationView

---
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,64 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // Background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        HomeView()
+            .toolbar(.hidden) // For iOS 16+, navigation bar is automatically hidden if there is no navigation title
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background {
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                }
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+        
+    }
+}
