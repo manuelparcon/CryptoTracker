27549161e0772192104fc51a8127b7f9cd3114df
Manuel <manuel@manuelparcon.com>
2024-11-13 13:16:08 +0800

    Add CoinRowView, and Double and PreviewProvider extensions

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,73 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/13/24.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            centerColumn
+            rightColumn
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: true)
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack() {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var centerColumn: some View {
+        VStack {
+            if showHoldingsColumn {
+                VStack(alignment: .trailing) {
+                    Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                        .bold()
+                    Text((coin.currentHoldings ?? 0).asNumberString())
+                }
+                .foregroundStyle(Color.theme.accent)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+    
+}
