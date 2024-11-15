c6f4d972f21eb378886101fe4dee0ef23662dd16
Manuel <manuel@manuelparcon.com>
2024-11-15 13:33:28 +0800

    Add CoinDataService, downloading coins using a View Model and Data Service class

---
+++ CryptoTracker/Services/CoinDataService.swift
@@ -0,0 +1,52 @@
+//
+//  CoinDataServices.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/14/24.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = [] // Using @Published makes allCoins a publisher that can have subscribers
+//    var cancellables = Set<AnyCancellable>()
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        
+        // Download using combine
+//        URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins // self needs to be weak self
+                self?.coinSubscription?.cancel()
+            }
+//            .store(in: &cancellables)
+
+    }
+}
