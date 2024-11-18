083b8083cf2f28e00e2f66f1dd659bbb4fcd8ec3
Manuel <manuel@manuelparcon.com>
2024-11-18 12:18:28 +0800

    Add downloading Coin Images using Combine, MVVM, and a Networking Layer

---
+++ CryptoTracker/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageView.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/16/24.
+//
+
+import SwiftUI
+
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin)) // _vm references the StateObject of this view model
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinImageView(coin: DeveloperPreview.instance.coin)
+}
