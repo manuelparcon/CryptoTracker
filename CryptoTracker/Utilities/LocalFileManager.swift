18259ad334557d24c70b5e9818c390ea76a6d85d
Manuel <manuel@manuelparcon.com>
2024-11-18 22:39:54 +0800

    Add LocalFileManager - Saving images to the File Manager

---
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -0,0 +1,73 @@
+//
+//  LocalFileManager.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() { }
+    
+    // Save images to file manager
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        
+        // Create folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        // Get path for image
+        guard
+            let data = image.pngData(),
+            let url = getURLForImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        // Save image to path
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving image. Image Name: \(imageName). Error: \(error)")
+        }
+    }
+    
+    // Get image back after saving
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        
+        guard
+            let url = getURLForImage(imageName: imageName, folderName: folderName),
+            FileManager.default.fileExists(atPath: url.path) else { return nil }
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    // Create folder if not yet created
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path()) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating directory. Folder Name: \(folderName). Error: \(error)")
+            }
+        }
+    }
+    
+    // Get url for the folder we want to get into
+    private func getURLForFolder(folderName: String) -> URL? {
+        
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
+        return url.appending(path: folderName, directoryHint: .isDirectory)
+    }
+    
+    // Get url where we will save image inside the folder
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderURL = getURLForFolder(folderName: folderName) else { return nil }
+        return folderURL.appending(path: imageName + ".png", directoryHint: .notDirectory)
+    }
+    
+    // Get images back from file manager
+    
+}
