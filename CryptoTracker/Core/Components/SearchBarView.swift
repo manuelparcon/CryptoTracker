66f0a6573644d4ee622fa7d4bae3f83a9d862eb8
Manuel <manuel@manuelparcon.com>
2024-11-19 22:04:39 +0800

    Add a custom and reusable search bar

---
+++ CryptoTracker/Core/Components/SearchBarView.swift
@@ -0,0 +1,53 @@
+//
+//  SearchBarView.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/19/24.
+//
+
+import SwiftUI
+
+// Reusable search bar
+struct SearchBarView: View {
+
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol", text: $searchText)
+                .foregroundStyle(Color.theme.accent)
+                .autocorrectionDisabled(true)
+                .keyboardType(.alphabet)
+                .overlay(alignment: .trailing) {
+                    Image(systemName: "xmark.circle.fill")
+                        .padding() // add padding to increase tappable area
+                        .offset(x: 10) // push to the right to offset added padding
+                        .foregroundStyle(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        }
+                }
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(
+                    color: Color.theme.accent.opacity(0.15),
+                    radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    SearchBarView(searchText: .constant(""))
+}
---
+++ CryptoTracker/Extensions/UIApplication.swift
@@ -0,0 +1,17 @@
+//
+//  UIApplication.swift
+//  CryptoTracker
+//
+//  Created by Manuel Parcon on 11/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIApplication {
+    
+    
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
